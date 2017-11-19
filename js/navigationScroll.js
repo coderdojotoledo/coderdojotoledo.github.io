c3e729b11e481e84c33a8d1c4ab2fb91f4752944
danpaquette <dm.paquette@gmail.com>
2017-11-19 13:21:42 -0500

    Committed new site files.

---
+++ js/navigationScroll.js
@@ -0,0 +1,10 @@
+var navigationScroll = function() {
+
+  "use strict";
+
+  // Scroll from top navigation to hashed section.
+
+  $('a[href^="#"]').click(function() {
+    $.scrollTo($(this).attr('href'), 800);
+  });
+}();
